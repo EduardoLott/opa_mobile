3374ba10d1cdbd6aea301de19f6804ac774ef310
MateusQ7 <mateusquixada@gmail.com>
2024-03-25 19:17:51 -0300

    fix: Ajusta cor da yellow

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color brownOpa = Color(0xFF956004); // MARROM OPA
-const Color yellowOpa = Color(0xFFFFC107); // MARROM OPA
+const Color yellowOpa = Color(0xFFFFCF24); // MARROM OPA
