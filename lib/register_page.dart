f165124f466dcb85fab8edfa87f19ca1aed4bf95
MateusQ7 <mateusquixada@gmail.com>
2024-03-26 14:46:42 -0300

    fix: Refatora tela de Registro

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -3,95 +3,126 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'colors.dart';
 
+// ignore: must_be_immutable
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({Key? key});
+  RegisterPage({super.key, Key});
+
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2101),
+    );
+    if (pickedDate != null && pickedDate != selectedDate) {
+      setState(() {
+        selectedDate = pickedDate;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        centerTitle: true,
-        title: SizedBox(
-          width: 85,
-          child: Image.asset('assets/OPA_logo.png'),
-        ),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.all(27),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.white,
-              Colors.white70,
-            ],
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          centerTitle: true,
+          title: SizedBox(
+            width: 85,
+            child: Image.asset('assets/OPA_logo.png'),
           ),
+          elevation: 0,
+          backgroundColor: yellowOpa,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Stack(
           children: [
-            _buildTextFieldWithLabel(
-              context,
-              labelText: 'Nome',
-              placeholder: 'Digite o seu nome completo',
-            ),
-            const SizedBox(height: 5),
-            Row(
-              children: [
-                Expanded(
-                  child: _buildDateInputWithLabel(
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.all(27),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.white,
+                    Colors.white70,
+                  ],
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildTextFieldWithLabel(
                     context,
-                    labelText: 'Data',
+                    labelText: 'Nome',
+                    placeholder: 'Digite o seu nome completo',
                   ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: _buildGenderDropdownWithLabel(
+                  const SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: _buildDateInputWithLabel(
+                          context,
+                          labelText: 'Data',
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: _buildGenderDropdownWithLabel(
+                          context,
+                          labelText: 'Gênero',
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  _buildTextFieldWithLabel(
                     context,
-                    labelText: 'Gênero',
+                    labelText: 'Senha',
+                    placeholder: 'Digite sua senha',
+                    obscureText: true,
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 5),
-            _buildTextFieldWithLabel(
-              context,
-              labelText: 'Senha',
-              placeholder: 'Digite sua senha',
-              obscureText: true,
-            ),
-            const SizedBox(height: 5),
-            _buildTextFieldWithLabel(
-              context,
-              labelText: 'Confirmar Senha',
-              placeholder: 'Confirme sua senha',
-              obscureText: true,
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              width: double.infinity,
-              child: CupertinoButton(
-                padding: const EdgeInsets.all(17),
-                color: Colors.yellow,
-                child: const Text(
-                  'Cadastrar',
-                  style: TextStyle(
-                    color: Colors.brown,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
+                  const SizedBox(height: 5),
+                  _buildTextFieldWithLabel(
+                    context,
+                    labelText: 'Confirmar Senha',
+                    placeholder: 'Confirme sua senha',
+                    obscureText: true,
                   ),
-                ),
-                onPressed: () {},
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    child: CupertinoButton(
+                      padding: const EdgeInsets.all(17),
+                      color: Colors.yellow,
+                      child: Text(
+                        'CADASTRAR',
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                              color: Colors.brown,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 3.0),
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  const SizedBox(height: 7),
+                ],
               ),
             ),
-            const SizedBox(height: 7),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                height: 70, // Altura do quadrado
+                color: const Color(0xFFF8E49E), // Cor do quadrado
+              ),
+            ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 
   Widget _buildTextFieldWithLabel(BuildContext context,
@@ -101,21 +132,23 @@ class RegisterPage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          labelText,
-          style: const TextStyle(
-            color: Color(0xFF525252),
-            fontSize: 14,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        Text(labelText,
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(
+                color: Color(0xFF525252),
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
         CupertinoTextField(
           padding: const EdgeInsets.all(15),
           cursorColor: Colors.yellow,
           placeholder: placeholder,
           obscureText: obscureText,
-          placeholderStyle:
-              const TextStyle(color: Color(0xFF818181), fontSize: 16),
+          placeholderStyle: const TextStyle(
+              color: Color(0xFF818181),
+              fontSize: 16,
+              fontWeight: FontWeight.w300),
           style: GoogleFonts.poppins(
             textStyle: const TextStyle(
                 color: Color(0xFF818181),
@@ -123,7 +156,7 @@ class RegisterPage extends StatelessWidget {
                 fontWeight: FontWeight.w300),
           ),
           decoration: BoxDecoration(
-            border: Border.all(color: const Color(0xFF818181), width: 2.0),
+            border: Border.all(color: const Color(0xFFD0D0D0), width: 2.0),
             color: const Color(0xFFF8F8F8),
             borderRadius: const BorderRadius.all(
               Radius.circular(6),
@@ -139,22 +172,22 @@ class RegisterPage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          labelText,
-          style: const TextStyle(
-            color: Color(0xFF525252),
-            fontSize: 14,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        Text(labelText,
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(
+                color: Color(0xFF525252),
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
         GestureDetector(
           onTap: () {
-            // Implementar a lógica do DatePicker
+            _selectDate(context);
           },
           child: Container(
             padding: const EdgeInsets.all(15),
             decoration: BoxDecoration(
-              border: Border.all(color: const Color(0xFF818181), width: 2.0),
+              border: Border.all(color: const Color(0xFFD0D0D0), width: 2.0),
               color: const Color(0xFFF8F8F8),
               borderRadius: const BorderRadius.all(
                 Radius.circular(6),
@@ -163,15 +196,14 @@ class RegisterPage extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  'Selecionar data',
-                  style: const TextStyle(
-                    color: Color(0xFF818181),
-                    fontSize: 14,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-                Icon(
+                Text('Data',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                          color: Color(0xFF525252),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w300),
+                    )),
+                const Icon(
                   Icons.calendar_today,
                   color: Color(0xFF818181),
                 ),
@@ -188,22 +220,21 @@ class RegisterPage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          labelText,
-          style: const TextStyle(
-            color: Color(0xFF525252),
-            fontSize: 14,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        Text(labelText,
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(
+                color: Color(0xFF525252),
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
         DropdownButtonFormField<String>(
           decoration: InputDecoration(
             contentPadding:
                 const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(6),
-              borderSide:
-                  BorderSide(color: const Color(0xFF818181), width: 2.0),
+              borderSide: BorderSide(color: Color(0xFFD0D0D0), width: 2.0),
             ),
             fillColor: const Color(0xFFF8F8F8),
             filled: true,
@@ -211,21 +242,20 @@ class RegisterPage extends StatelessWidget {
           items: ['Masculino', 'Feminino', 'Outro']
               .map((gender) => DropdownMenuItem<String>(
                     value: gender,
-                    child: Text(
-                      gender,
-                      style: const TextStyle(
-                        color: Color(0xFF818181),
-                        fontSize: 16,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
+                    child: Text(gender,
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                              color: Color(0xFF525252),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w300),
+                        )),
                   ))
               .toList(),
-          onChanged: (String? value) {
-            // Implementar a lógica de seleção do gênero
-          },
+          onChanged: (String? value) {},
         ),
       ],
     );
   }
+
+  void setState(Null Function() param0) {}
 }
