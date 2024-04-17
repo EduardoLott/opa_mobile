a71a7b9d90568897d924d731d0550315c3d98ab3
MateusQ7 <mateusquixada@gmail.com>
2024-04-17 00:28:23 -0300

    feat: Adiciona form ao register

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -4,8 +4,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'colors.dart';
 
 // ignore: must_be_immutable
-class RegisterPage extends StatelessWidget {
-  RegisterPage({super.key, Key});
+class RegisterPage extends StatefulWidget {
+  RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
 
   DateTime selectedDate = DateTime.now();
 
@@ -34,105 +41,136 @@ class RegisterPage extends StatelessWidget {
           child: Image.asset('assets/OPA_logo.png'),
         ),
         elevation: 0,
-        backgroundColor: yellowOpa,
+        backgroundColor: Colors.yellow, // Alteração para a cor amarela
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.only(
-            top: MediaQuery.of(context).padding.top +
-                kToolbarHeight), // Adiciona padding superior
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.all(27),
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.white,
-                Colors.white70,
-              ],
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _buildTextFieldWithLabel(
-                context,
-                labelText: 'Nome',
-                placeholder: 'Digite o seu nome completo',
-              ),
-              const SizedBox(height: 5),
-              _buildTextFieldWithLabel(
-                context,
-                labelText: 'Email',
-                placeholder: 'Digite o seu email',
-              ),
-              const SizedBox(height: 5),
-              _buildTextFieldWithLabel(
-                context,
-                labelText: 'Usuário',
-                placeholder: 'Digite o seu usuário',
-              ),
-              const SizedBox(height: 5),
-              _buildTextFieldWithLabel(
-                context,
-                labelText: 'CPF',
-                placeholder: 'Digite o seu CPF',
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: [
-                  Expanded(
-                    child: _buildDateInputWithLabel(
-                      context,
-                      labelText: 'Data de nascimento',
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: _buildGenderDropdownWithLabel(
-                      context,
-                      labelText: 'Gênero',
-                    ),
-                  ),
+          top: MediaQuery.of(context).padding.top + kToolbarHeight,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.all(27),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.white,
+                  Colors.white70,
                 ],
               ),
-              const SizedBox(height: 5),
-              _buildTextFieldWithLabel(
-                context,
-                labelText: 'Senha',
-                placeholder: 'Digite sua senha',
-                obscureText: true,
-              ),
-              const SizedBox(height: 5),
-              _buildTextFieldWithLabel(
-                context,
-                labelText: 'Confirmar Senha',
-                placeholder: 'Confirme sua senha',
-                obscureText: true,
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: double.infinity,
-                child: CupertinoButton(
-                  padding: const EdgeInsets.all(17),
-                  color: yellowOpa,
-                  child: Text(
-                    'CADASTRAR',
-                    style: TextStyle(
-                      color: Colors.black, // Define a cor do texto como preto
-                    ),
-                  ),
-                  onPressed: () {},
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Nome',
+                  placeholder: 'Digite o seu nome completo',
                 ),
-              ),
-              const SizedBox(height: 7),
-            ],
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Email',
+                  placeholder: 'Digite o seu email',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Usuário',
+                  placeholder: 'Digite o seu usuário',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'CPF',
+                  placeholder: 'Digite o seu CPF',
+                ),
+                const SizedBox(height: 5),
+                _buildDateInputWithLabel(
+                  context,
+                  labelText: 'Data de nascimento',
+                ),
+                const SizedBox(height: 5),
+                _buildGenderDropdownWithLabel(
+                  context,
+                  labelText: 'Gênero',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Telefone',
+                  placeholder: 'Digite o seu número de telefone',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'CEP',
+                  placeholder: 'Digite o seu CEP',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Cidade',
+                  placeholder: 'Digite o nome da sua cidade',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Bairro',
+                  placeholder: 'Digite o nome do seu bairro',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Estado',
+                  placeholder: 'Digite a sigla do seu estado',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Rua',
+                  placeholder: 'Digite o nome da sua rua',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Número',
+                  placeholder: 'Digite o número da sua residência',
+                ),
+                const SizedBox(height: 5),
+                _buildTextFormFieldWithLabel(
+                  context,
+                  labelText: 'Complemento',
+                  placeholder: 'Digite o complemento (opcional)',
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: double.infinity,
+                  child: CupertinoButton(
+                    padding: const EdgeInsets.all(17),
+                    color: Colors.yellow,
+                    child: const Text(
+                      'CADASTRAR',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                    },
+                  ),
+                ),
+                const SizedBox(height: 7),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Widget _buildTextFieldWithLabel(BuildContext context,
+  Widget _buildTextFormFieldWithLabel(BuildContext context,
       {required String labelText,
       required String placeholder,
       bool obscureText = false}) {
@@ -147,27 +185,27 @@ class RegisterPage extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             )),
-        CupertinoTextField(
-          padding: const EdgeInsets.all(15),
-          cursorColor: Colors.yellow,
-          placeholder: placeholder,
-          obscureText: obscureText,
-          placeholderStyle: const TextStyle(
-              color: Color(0xFF818181),
-              fontSize: 16,
-              fontWeight: FontWeight.w300),
-          style: GoogleFonts.poppins(
-            textStyle: const TextStyle(
+        TextFormField(
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Por favor, digite seu $labelText';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.all(15),
+            hintText: placeholder,
+            hintStyle: const TextStyle(
                 color: Color(0xFF818181),
                 fontSize: 16,
                 fontWeight: FontWeight.w300),
-          ),
-          decoration: BoxDecoration(
-            border: Border.all(color: const Color(0xFFD0D0D0), width: 2.0),
-            color: const Color(0xFFF8F8F8),
-            borderRadius: const BorderRadius.all(
-              Radius.circular(6),
+            border: OutlineInputBorder(
+              borderSide:
+                  const BorderSide(color: Color(0xFFD0D0D0), width: 2.0),
+              borderRadius: BorderRadius.circular(6),
             ),
+            fillColor: const Color(0xFFF8F8F8),
+            filled: true,
           ),
         ),
       ],
@@ -194,7 +232,7 @@ class RegisterPage extends StatelessWidget {
           child: Container(
             padding: const EdgeInsets.all(15),
             decoration: BoxDecoration(
-              border: Border.all(color: const Color(0xFFD0D0D0), width: 2.0),
+              border: Border.all(color: Color(0xFFD0D0D0), width: 2.0),
               color: const Color(0xFFF8F8F8),
               borderRadius: const BorderRadius.all(
                 Radius.circular(6),
@@ -236,6 +274,12 @@ class RegisterPage extends StatelessWidget {
               ),
             )),
         DropdownButtonFormField<String>(
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Por favor, selecione seu gênero';
+            }
+            return null;
+          },
           decoration: InputDecoration(
             contentPadding:
                 const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
@@ -243,7 +287,7 @@ class RegisterPage extends StatelessWidget {
               borderRadius: BorderRadius.circular(6),
               borderSide: BorderSide(color: Color(0xFFD0D0D0), width: 2.0),
             ),
-            fillColor: const Color(0xFFF8F8F8),
+            fillColor: Color(0xFFF8F8F8),
             filled: true,
           ),
           items: ['Masculino', 'Feminino', 'Outro']
@@ -263,6 +307,4 @@ class RegisterPage extends StatelessWidget {
       ],
     );
   }
-
-  void setState(Null Function() param0) {}
 }
