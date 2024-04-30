16043cf7ded96116de28d31f60da0544e9d247ad
MateusQ7 <mateusquixada@gmail.com>
2024-04-29 23:37:52 -0300

    feat: Added UserData

---
+++ lib/data/UserData.dart
@@ -0,0 +1,55 @@
+class UserData {
+  String username;
+  String password;
+  String email;
+  String name;
+  String gender;
+  String cpf;
+  String phoneNumber;
+  String street;
+  String neighborhood;
+  String streetNumber;
+  String complement;
+  String city;
+  String state;
+  String cep;
+  String birthDate;
+
+  UserData({
+    required this.username,
+    required this.password,
+    required this.email,
+    required this.name,
+    required this.gender,
+    required this.cpf,
+    required this.phoneNumber,
+    required this.street,
+    required this.neighborhood,
+    required this.streetNumber,
+    required this.complement,
+    required this.city,
+    required this.state,
+    required this.cep,
+    required this.birthDate,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'username': username,
+      'password': password,
+      'email': email,
+      'name': name,
+      'gender': gender,
+      'cpf': cpf,
+      'phoneNumber': phoneNumber,
+      'street': street,
+      'neighborhood': neighborhood,
+      'streetNumber': streetNumber,
+      'complement': complement,
+      'city': city,
+      'state': state,
+      'cep': cep,
+      'birthDate': birthDate,
+    };
+  }
+}
