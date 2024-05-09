e971ff02789947fcd6b78edbf9dc8bcda5db23fd
MateusQ7 <mateusquixada@gmail.com>
2024-05-09 15:06:31 -0300

    feat: Adiciona classe de Service para o CEP

---
+++ lib/service/CepService.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class CepService {
+  static Future<Map<String, dynamic>> fetchAddress(String cep) async {
+    final response =
+        await http.get(Uri.parse('https://viacep.com.br/ws/${cep}/json/'));
+
+    if (response.statusCode == 200) {
+      final parsedJson = jsonDecode(response.body);
+      return {
+        'street': parsedJson['logradouro'],
+        'neighborhood': parsedJson['bairro'],
+        'city': parsedJson['localidade'],
+        'state': parsedJson['uf'],
+      };
+    } else if (response.statusCode == 404) {
+      throw Exception('CEP não encontrado');
+    } else {
+      throw Exception('Falha ao buscar o endereço');
+    }
+  }
+}
