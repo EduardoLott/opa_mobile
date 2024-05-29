e608ff18f53626958d9e28a800b09dbc93f7dfcf
MateusQ7 <mateusquixada@gmail.com>
2024-05-29 14:10:09 -0300

    fix: Adiciona int ao parametro da função tokenToBack e coloca 1 no customerId

--- lib/pages/token-page/token_page_service.dart
+++ lib/pages/token-page/token_page_service.dart
@@ -3,27 +3,20 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:opamobile/services/auth_service.dart';
 import 'package:opamobile/services/user_service.dart';
-class TokenPageService{
 
-  static Future<dynamic> tokenToBack(String token) async {
+class TokenPageService {
+  static Future<dynamic> tokenToBack(int token) async {
     try {
       final userToken = AuthService.getUserToken();
       final response = await http.post(
-        Uri.parse(
-            'http://192.168.0.36:3000/table/add-customer'),
-        headers: <String, String>{
-          'Content-Type':
-              'application/json; charset=UTF-8',
-              'Authorization': 'Bearer $userToken', // Passar o token nos headers
-        },
-        body: jsonEncode({ 
-          'tableId':token,
-          'customerId': UserSingleton.getUserInfo().id
-        })
-      );
+          Uri.parse('http://192.168.0.36:3000/table/add-customer'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer $userToken', // Passar o token nos headers
+          },
+          body: jsonEncode({'tableId': token, 'customerId': 1}));
       return response;
-    }
-    catch(e){
+    } catch (e) {
       print("$e");
     }
   }
