df40e811658fed1b44e2f7241a999c85f8952b0e
MateusQ7 <mateusquixada@gmail.com>
2024-06-13 11:26:24 -0300

    fix

--- lib/pages/menu/menu_page.dart
+++ lib/pages/menu/menu_page.dart
@@ -1,9 +1,6 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:opamobile/pages/menu/menu_interface.dart';
-import 'package:opamobile/pages/menu/menu_service.dart';
 import 'package:opamobile/shared/menucarditem.dart';
 import 'package:opamobile/utils/opa_colors.dart';
 
@@ -14,31 +11,35 @@ class MenuPage extends StatefulWidget {
   _MenuPageState createState() => _MenuPageState();
 }
 
-class _MenuPageState extends State<MenuPage>{
-  
-  late List<Menu> _menu = [
-    Menu(id:1,name:'carlos',price:30.99,description: 'carlosin carlosos carlaosdaoskdo3k3o'),
-    Menu(id:1,name:'carlos',price:30.99,description: 'carlosin carlosos carlaosdaoskdo3k3o'),
-    Menu(id:1,name:'carlos',price:30,description: 'carlosin carlosos carlaosdaoskdo3k3o'),
-    Menu(id:1,name:'carlos',price:30.99,description: 'carlosin carlosos carlaosdaoskdo3k3ocarlaosdaoskdo3k3ocarlaosdaoskdo3k3ocarlaosdaoskdo3k3o'),
+class _MenuPageState extends State<MenuPage> {
+  final List<Menu> _menu = [
+    Menu(
+        id: 1,
+        name: 'carlos',
+        price: 30.99,
+        description: 'carlosin carlosos carlaosdaoskdo3k3o'),
+    Menu(
+        id: 2,
+        name: 'maria',
+        price: 25.50,
+        description: 'Descrição do prato de Maria'),
+    Menu(
+        id: 3,
+        name: 'joão',
+        price: 40.00,
+        description: 'Descrição do prato de João'),
+    Menu(
+        id: 4,
+        name: 'ana',
+        price: 35.75,
+        description: 'Descrição do prato de Ana'),
   ];
 
   @override
   void initState() {
-    // rascunho de requisicao assim que entrar na rota e com o async no nome da funcao 
-    getMenu();
+    // rascunho de requisicao assim que entrar na rota e com o async no nome da funcao
+    // _menu = await MenuService.getMenu();
     super.initState();
-  }
-
-  Future<void> getMenu () async{
-    try {
-      var menuInfoFromService = await MenuService.getMenu();
-      setState(() {
-        _menu = menuInfoFromService as List<Menu>;
-      });
-    } catch (e) {
-      print("Error fetching table info: $e");
-    }
   }
 
   @override
@@ -46,51 +47,60 @@ class _MenuPageState extends State<MenuPage>{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          backgroundColor: OpaColors.yellowOpa,
-          automaticallyImplyLeading: true, // tirar o botão de voltar
-          actions: [],
-          title: Center(
-            child: SizedBox(
-              width: 85,
-              child: Image.asset('assets/OPA_logo.png'),
-            ),
-          ),
-        ),
-        resizeToAvoidBottomInset:
-          false,
-        body:SingleChildScrollView(
-          // physics: ScrollPhysics(),
-          child: Column(
-            children:[
-              Text(
-                'MENU',
-                style: GoogleFonts.poppins(
-                  textStyle: const TextStyle(
-                  color:OpaColors.graytext,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              backgroundColor: OpaColors.yellowOpa,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              title: Center(
+                child: SizedBox(
+                  width: 85,
+                  child: Image.asset('assets/OPA_logo.png'),
                 ),
               ),
-              ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: _menu.length,
-                  itemBuilder: (context, index) {
-                    final menuItem = _menu[index];
-                    return MenuCardItem(name: menuItem.name, price: menuItem.price, description: menuItem.description,);
-                  }
-                )
-            ],
-          ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'MENU',
+                  style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                      color: OpaColors.graytext,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  final menuItem = _menu[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8.0),
+                    child: MenuCardItem(
+                      name: menuItem.name,
+                      price: menuItem.price,
+                      description: menuItem.description,
+                    ),
+                  );
+                },
+                childCount: _menu.length,
+              ),
+            ),
+          ],
         ),
-      )
+      ),
     );
-  } 
+  }
 }
--- lib/shared/menucarditem.dart
+++ lib/shared/menucarditem.dart
@@ -1,76 +1,73 @@
-
-
 import 'package:flutter/material.dart';
 
-class MenuCardItem extends StatelessWidget{
+class MenuCardItem extends StatelessWidget {
   final String name;
   final double price;
-  final String? description;
+  final String description;
 
-  const MenuCardItem({super.key, required this.name,required this.price,this.description});
+  const MenuCardItem({
+    Key? key,
+    required this.name,
+    required this.price,
+    required this.description,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300,
-      height: 90,
-      child: Card(
-        elevation: 4, 
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16), 
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child:Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Image.asset(
-                'assets/productitem100x100.png',
-              ),
-              Column(
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 72.0,
+              height: 72.0,
+              color: Colors.grey[300], // Placeholder para uma imagem
+              child: Icon(Icons.fastfood, size: 48),
+            ),
+            const SizedBox(width: 8.0),
+            Expanded(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      SizedBox(
-                        height: 20,
-                        width: 250,
+                      Expanded(
                         child: Text(
                           name,
-                          style: const TextStyle(
-                            fontSize: 18,
+                          style: TextStyle(
                             fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
                           ),
+                          overflow: TextOverflow.ellipsis,
                         ),
                       ),
+                      const SizedBox(width: 8.0),
                       Text(
-                        'R\$$price' ,
-                        style: const TextStyle(
-                          fontSize: 18,
+                        'R\$${price.toStringAsFixed(2)}',
+                        style: TextStyle(
                           fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
                         ),
-                      )
+                      ),
                     ],
                   ),
-                  SizedBox(
-                    height: 40,
-                    width: 250, 
-                    child: Text(
-                      description!,
-                      style: const TextStyle(
-                        fontSize: 15,
-                      ),
-                    ),
-                  )
+                  const SizedBox(height: 4.0),
+                  Text(
+                    description,
+                    style: TextStyle(fontSize: 15.0),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
                 ],
               ),
-            ],
-          ) ,
+            ),
+          ],
         ),
       ),
     );
   }
-  
 }
